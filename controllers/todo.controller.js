6d990bb0e424900fdfbca3526643b82511c10cf6
Mohamed <mahameds19999@gmail.com>
2021-02-01 13:55:58 +0200

    Add the rest routes

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,10 +1,18 @@
 const router = require("koa-router");
-const { getTodoList } = require("../services/todo-list.service");
+const {
+  getTodoList,
+  addTodo,
+  markTodoAsCompleted,
+  deleteTodo,
+} = require("../services/todo-list.service");
 
 const app = new router({
   prefix: "/todo",
 });
 
 app.get("/list", getTodoList);
+app.post("/add", addTodo);
+app.put("/:id", markTodoAsCompleted);
+app.delete("/:id", deleteTodo);
 
 module.exports = app;
--- services/todo-list.service.js
+++ services/todo-list.service.js
@@ -1,9 +1,49 @@
-const todoList = require("../data/todo-list");
+let todoList = require("../data/todo-list");
 
 const getTodoList = (ctx) => {
   ctx.body = todoList;
 };
 
+const addTodo = (ctx) => {
+  const newTodo = {
+    id: generateTodoIncId(),
+    name: ctx.request.body.name,
+    done: false,
+  };
+  todoList.push(newTodo);
+  ctx.body = todoList;
+};
+
+const markTodoAsCompleted = (ctx) => {
+  const todoId = ctx.params.id;
+  let isTodoFound;
+
+  todoList.map((todoItem) => {
+    if (todoItem.id === +todoId) {
+      isTodoFound = true;
+      todoItem.done = true;
+      return;
+    }
+  });
+
+  if (!isTodoFound) ctx.throw(404, "No todo found");
+  else ctx.body = todoList;
+};
+
+const deleteTodo = (ctx) => {
+  const todoId = +ctx.params.id;
+  todoList = todoList.filter((todoItem) => todoItem.id !== todoId);
+  ctx.body = todoList;
+};
+
+generateTodoIncId = () => {
+  let lastTodoId = todoList[todoList.length - 1].id;
+  return ++lastTodoId;
+};
+
 module.exports = {
   getTodoList,
+  addTodo,
+  markTodoAsCompleted,
+  deleteTodo,
 };
